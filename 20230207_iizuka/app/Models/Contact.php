764caddb7bfe3920f42669e3748593811483f361
hidepon503 <blue.passion625@gmail.com>
2023-02-07 16:05:25 +0900

    AdvancedTest

--- 20230207_iizuka/app/Models/Contact.php
+++ 20230207_iizuka/app/Models/Contact.php
@@ -10,33 +10,6 @@ class Contact extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
-/*
-    public static function doSearch($fullname,$gender,$from,$until,$email)
-    {
-        $query = self::query();
-        if(!empty($fullname)){
-            $query->where('fullname', 'like binary', "%{$fullname}%");
-        }
-        $query = self::query();
-        if(!empty($gender)){
-            $query->where('gender', '=', "%{$gender}%");
-        }
-        $query = self::query();
-        if(!empty($from)){
-            $query->where('created_at', '=', "%{$from}%");
-        }
-        $query = self::query();
-        if(!empty($until)){
-            $query->where('created_at', '=', "%{$until}%");
-        }
-        $query = self::query();
-        if(!empty($email)){
-            $query->where('email', 'like binary', "%{$email}%");
-        }
-        $results = $query->get();
-        return $results;
-    }
-*/
 
     public static function getDate($from, $until)
     {
